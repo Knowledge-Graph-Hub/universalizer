b2a52b2ce2ebccdde9685665f41aa2f003096b5d
caufieldjh <j.harry.caufield@gmail.com>
2022-09-22 16:07:26 -0400

    Setup CLI

---
+++ universalizer/cli.py
@@ -0,0 +1,25 @@
+"""CLI for universalizer."""
+
+import click
+
+
+@click.group()
+def cli():
+    """Click CLI for universalizer."""
+    pass
+
+
+@cli.command()
+@click.argument("input_path", type=click.Path(exists=True))
+@click.option("--compressed", "-c", required=False, default=False, is_flag=True)
+def run(input_path: str, compressed: bool) -> None:
+    """Process a graph, normalizing all nodes
+    :param input_path: Path to a directory containing
+    KGX format graph node and edge files, or a single
+    file if the compressed flag is used.
+    :param compressed: bool, True if input_path is a single .tar.gz
+    :return: None
+    """
+    print(f"Input path: {input_path}")
+
+    return None
