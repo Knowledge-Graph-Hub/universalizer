2537cc8451c4a541aa93a41e31db9eeadae1c892
caufieldjh <j.harry.caufield@gmail.com>
2022-09-28 17:28:55 -0400

    Set up CLI for map input

--- README.md
+++ README.md
@@ -24,3 +24,21 @@ Or, if they're in a single tar.gz file:
 ```
 universalizer run -c graph.tar.gz
 ```
+
+### ID and category mapping
+
+SSSOM-format maps are supported. Use a single map file:
+
+```
+univeralizer run -m poro-mp-exact-1.0.sssom.tsv path/to/directory
+```
+
+or a whole directory of them:
+
+```
+univeralizer run -m path/to/mapfiles path/to/directory
+```
+
+For SSSOM maps from `subject_id` to `object_id`, subject node IDs will be remapped to object IDs.
+
+If the `object_id` is a Biolink category ID, however, the node's category ID will be remapped instead of its URI.
--- universalizer/cli.py
+++ universalizer/cli.py
@@ -15,18 +15,21 @@ def cli():
 @click.argument("input_path", type=click.Path(exists=True))
 @click.option("--compressed", "-c", required=False, default=False,
               is_flag=True)
-def run(input_path: str, compressed: bool) -> None:
+@click.option("--map_path", "-m", required=False)
+def run(input_path: str, compressed: bool, map_path: str) -> None:
     """Process a graph, normalizing all nodes.
 
     :param input_path: Path to a directory containing
     KGX format graph node and edge files, or a single
     file if the compressed flag is used.
     :param compressed: bool, True if input_path is a single .tar.gz
+    :param map_path: str, path to a single SSSOM ID map or
+    a directory of SSSOM maps.
     :return: None
     """
     print(f"Input path: {input_path}")
 
-    if clean_and_normalize_graph(input_path, compressed):
+    if clean_and_normalize_graph(input_path, compressed, map_path):
         print("Complete.")
 
     return None
