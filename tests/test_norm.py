03a38b5f85ebd93f37b27d5867440f15518dab5d
caufieldjh <j.harry.caufield@gmail.com>
2022-10-03 02:01:02 -0400

    Update tests

--- tests/test_norm.py
+++ tests/test_norm.py
@@ -10,12 +10,16 @@ class TestNorm(TestCase):
 
     def setUp(self) -> None:
         """Set up."""
-        self.test_graph_path = "tests/resources/"
-        self.test_graph_path_nodes = "tests/resources/test_nodes.tsv"
-        self.test_graph_path_edges = "tests/resources/test_edges.tsv"
+        self.test_graph_path = "tests/resources/graph_simple_test/"
+        self.test_graph_path_nodes = \
+            "tests/resources/graph_simple_test/test_nodes.tsv"
+        self.test_graph_path_edges = \
+            "tests/resources/graph_simple_test/test_edges.tsv"
 
     def test_clean_and_normalize_graph(self):
         """Test clean_and_normalize_graph."""
         self.assertTrue(
-            clean_and_normalize_graph(self.test_graph_path, compressed=False)
+            clean_and_normalize_graph(self.test_graph_path,
+                                      compressed=False,
+                                      maps=[])
         )
