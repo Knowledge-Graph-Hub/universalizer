fd46a83afe72c66003acfa57752397197d15ea7d
caufieldjh <j.harry.caufield@gmail.com>
2023-03-01 15:03:42 -0500

    Add test for removing extra edges

--- tests/test_norm.py
+++ tests/test_norm.py
@@ -13,6 +13,7 @@ class TestNorm(TestCase):
         self.test_graph_path = "tests/resources/graph_simple_test/"
         self.id_graph_path = "tests/resources/graph_for_ids/"
         self.cat_graph_path = "tests/resources/graph_for_cats/"
+        self.chebi_graph_path = "tests/resources/graph_chebi/"
         self.map_paths = [
             "tests/resources/mappings/ogg-gene-test-0.1.sssom.tsv",
             "tests/resources/mappings/skos-biolink_cats-all-1.0.sssom.tsv",
@@ -54,6 +55,22 @@ class TestNorm(TestCase):
                 compressed=False,
                 maps=self.map_paths,
                 update_categories=False,
+                contexts=["obo", "bioregistry.upper"],
+                namespace_cat_map="",
+                oak_lookup=False,
+            )
+        )
+
+    def test_normalize_and_remove_extra_edges(self):
+        """Test clean_and_normalize_graph, in a case where
+        a node ID and its category are updated AND category
+        edges must be removed. This happens with PHENIO."""
+        self.assertTrue(
+            clean_and_normalize_graph(
+                self.chebi_graph_path,
+                compressed=False,
+                maps=[],
+                update_categories=True,
                 contexts=["obo", "bioregistry.upper"],
                 namespace_cat_map="",
                 oak_lookup=False,
